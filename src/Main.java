c2a7dbe92fcabb446fb99218d4dcca1e5446ece5
Scott <ScottWilliamsSolutions@gmail.com>
2023-10-02 20:32:52 -0400

    setting information without constructor

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Account scottsAccount = new Account();
 
+        scottsAccount.setNumber("123456789");
+        scottsAccount.setBalance(2000000);
+        scottsAccount.setCustomerName("Scott Williams");
+        scottsAccount.setCustomerEmail("Example@fake.com");
+        scottsAccount.setCustomerPhone("555-5555");
+
         scottsAccount.depositFunds(250);
         scottsAccount.withdrawFunds(50);
     }
