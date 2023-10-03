215966c208e8bf4a154b239924fe5b4e49835e74
Scott <ScottWilliamsSolutions@gmail.com>
2023-10-02 20:29:33 -0400

    Name change main => Main testing code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Account scottsAccount = new Account();
+
+        scottsAccount.depositFunds(250);
+        scottsAccount.withdrawFunds(50);
+    }
+
+
+
+}
